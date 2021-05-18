a8c3d88bf5050b58ebc54d9fd5eaaeb2e0764428
Nícolas Moura <nicolas_moura02@outlook.com>
2021-05-18 10:28:18 -0300

    Create basic app

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 5000;
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use((req, res) => {
+  const inspectValues = {
+    url: req.url,
+    method: req.method,
+    params: req.params,
+    body: req.body,
+    headers: req.headers,
+  };
+  console.log(inspectValues);
+  res.send({ inspectValues });
+});
+app.listen(PORT, () => {
+  console.log(`Running on PORT ${PORT}`);
+});
